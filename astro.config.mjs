cb10ad3a10935adb7720690e3edf796522620463
赵宇恒 <zhaoyuheng@travelsky.com.cn>
2024-10-08 14:58:38 +0800

    国际化

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,19 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			// 为此网站设置英语为默认语言。
+			defaultLocale: 'en',
+			locales: {
+			  // 英文文档在 `src/content/docs/en/` 中。
+			  en: {
+				label: 'English',
+			  },
+			  // 简体中文文档在 `src/content/docs/zh-cn/` 中。
+			  'zh-cn': {
+				label: '简体中文',
+				lang: 'zh-CN',
+			  },
+			},
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
